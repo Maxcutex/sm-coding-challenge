d1f336afaddd430bdb17d7e304a96c3563bde819
Eno Bassey <ennyboy@gmail.com>
2020-03-18 17:24:31 +0100

    add cache services and attributes for redis implementation

---
+++ Cache/RedisCacheSettings.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace sm_coding_challenge.Cache
+{
+    public class RedisCacheSettings
+    {
+        public bool Enabled { get; set; }
+
+        public string ConnectionString { get; set; }
+    }
+}
--- Models/PlayerModel.cs
+++ Models/PlayerModel.cs
@@ -13,6 +13,56 @@ namespace sm_coding_challenge.Models
 
         [DataMember(Name = "position")]
         public string Position { get; set; }
+
+        [DataMember(Name = "entry_id")]
+        public string EntryId { get; set; }
+        
+    }
+    [DataContract]
+    public class ReceivingPlayerModel : PlayerModel
+    {
+        /*
+         * "yds": "7",
+         "tds": "0",
+         "rec": "1"
+         */
+    }
+
+    [DataContract]
+    public class RushingPlayerModel : PlayerModel
+    {
+        [DataMember(Name = "yds")]
+        public string Yards { get; set; }
+        [DataMember(Name = "att")]
+        public string Att { get; set; }
+        [DataMember(Name = "tds")]
+        public string Tds { get; set; }
+        [DataMember(Name = "fum")]
+        public string Fum { get; set; }
+    }
+
+    [DataContract]
+    public class PassingPlayerModel : PlayerModel
+    {
+        /*
+         * "yds": "177",
+         "att": "23",
+         "tds": "1",
+         "cmp": "12",
+         "int": "1"
+         */
+    }
+
+    [DataContract]
+    public class KickingPlayerModel : PlayerModel
+    {
+        /*
+         *"fld_goals_made": "1",
+         "fld_goals_att": "1",
+         "extra_pt_made": "5",
+         "extra_pt_att": "5"
+         *
+         */
     }
 }
 
