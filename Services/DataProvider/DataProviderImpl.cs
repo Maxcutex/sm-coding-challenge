a6b2bb3cb6d22e3932921c29b0b42a4503c54324
Eno Bassey <ennyboy@gmail.com>
2020-03-18 16:01:54 +0100

    implement async await on functions and create new function for list

--- Services/DataProvider/DataProviderImpl.cs
+++ Services/DataProvider/DataProviderImpl.cs
@@ -1,6 +1,9 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using Newtonsoft.Json;
 using sm_coding_challenge.Models;
 
@@ -10,7 +13,7 @@ namespace sm_coding_challenge.Services.DataProvider
     {
         public static TimeSpan Timeout = TimeSpan.FromSeconds(30);
 
-        public PlayerModel GetPlayerById(string id)
+        public async Task<PlayerModel> GetPlayerById(string id)
         {
             var handler = new HttpClientHandler()
             {
@@ -53,6 +56,39 @@ namespace sm_coding_challenge.Services.DataProvider
                 }
             }
             return null;
+        }
+
+        public async Task<List<PlayerModel>> GetPlayersByIds(string[] listIds)
+        {
+            var playerList = new List<PlayerModel>();
+
+            var handler = new HttpClientHandler()
+            {
+                AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate
+            };
+            using (var client = new HttpClient(handler))
+            {
+                client.Timeout = Timeout;
+                var response = await client
+                        .GetAsync(
+                            "https://gist.githubusercontent.com/RichardD012/a81e0d1730555bc0d8856d1be980c803/raw/3fe73fafadf7e5b699f056e55396282ff45a124b/basic.json")
+                    ;
+                var stringData = response.Content.ReadAsStringAsync().Result;
+                var dataResponse = JsonConvert.DeserializeObject<DataResponseModel>(stringData,
+                    new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.Auto });
+
+                var receivingPlayers = dataResponse.Receiving.Where(x => listIds.Contains(x.Id)).ToList();
+                var rushingPlayers = dataResponse.Rushing.Where(x => listIds.Contains(x.Id)).ToList();
+                var passingPlayers = dataResponse.Passing.Where(x => listIds.Contains(x.Id)).ToList();
+                var kickingPlayers = dataResponse.Kicking.Where(x => listIds.Contains(x.Id)).ToList();
+
+                playerList.AddRange(receivingPlayers);
+                playerList.AddRange(rushingPlayers);
+                playerList.AddRange(passingPlayers);
+                playerList.AddRange(kickingPlayers);
+            }
+
+            return playerList;
         }
     }
 }
--- Services/DataProvider/IDataProvider.cs
+++ Services/DataProvider/IDataProvider.cs
@@ -1,9 +1,12 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using sm_coding_challenge.Models;
 
 namespace sm_coding_challenge.Services.DataProvider
 {
     public interface IDataProvider
     {
-        PlayerModel GetPlayerById(string id);
+        Task<PlayerModel> GetPlayerById(string id);
+        Task<List<PlayerModel>> GetPlayersByIds(string[] listIds);
     }
 }
